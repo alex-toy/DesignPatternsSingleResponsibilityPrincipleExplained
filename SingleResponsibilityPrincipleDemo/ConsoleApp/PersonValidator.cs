b1fa5c7a55705607f1f485124b670df1c009f456
alex-toy <alexei.80@hotmail.fr>
2023-03-05 14:34:50 +0100

    init

---
+++ SingleResponsibilityPrincipleDemo/ConsoleApp/PersonValidator.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleUI
+{
+    public class PersonValidator
+    {
+
+        public static bool Validate(Person person)
+        {
+            // Checks to be sure the first and last names are valid
+            bool firstNameWrong = string.IsNullOrWhiteSpace(person.FirstName);
+            if (firstNameWrong)
+            {
+                StandardMessages.DisplayValidationError("first name");
+                return false;
+            }
+
+            bool lastNameWrong = string.IsNullOrWhiteSpace(person.LastName);
+            if (lastNameWrong)
+            {
+                StandardMessages.DisplayValidationError("last name");
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ SingleResponsibilityPrincipleDemo/ConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StandardMessages.WelcomeMessage();
+
+            Person user = PersonDataCapture.Capture();
+
+            bool isUserValid = PersonValidator.Validate(user);
+            if (!isUserValid)
+            {
+                StandardMessages.EndApplication();
+                return;
+            }
+
+            AccountGenerator.CreateAccount(user);
+
+            StandardMessages.EndApplication();
+        }
+    }
+}
